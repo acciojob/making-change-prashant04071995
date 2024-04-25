b225b5abb57f59301e1492a7fee27400ac94e89e
Prashant Salve <128687203+prashant04071995@users.noreply.github.com>
2024-04-25 07:49:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 const makeChange = (c) => {
-  // your name here
+  const quarterValue = 25;
+  const dimeValue = 10;
+  const nickelValue = 5;
+  const pennyValue = 1;
+
+  let q = Math.floor(c / quarterValue);
+  c %= quarterValue;
+
+  let d = Math.floor(c / dimeValue);
+  c %= dimeValue;
+
+  let n = Math.floor(c / nickelValue); 
+  c %= nickelValue;
+
+  let p = c; // Remaining amount is the number of pennies
+
+  return { q, d, n, p };
 };
 
-// Do not the change the code below
 const c = prompt("Enter c: ");
 alert(JSON.stringify(makeChange(c)));
